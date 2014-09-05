dcec640fbc44077a1143d16a94fa278790f33dbe
Cameron Manderson <camm@flintinteractive.com.au>
2014-09-05 20:09:48 +1000

    Adds a test to getDataSource

--- tests/ActionTest.php
+++ tests/ActionTest.php
@@ -10,10 +10,45 @@ class ActionTest extends \PHPUnit_Framework_TestCase
         $action->setActionKernel($mockActionKernel);
         $this->assertNotEmpty($action->getActionKernel());
     }
+
+
+    public function testGetDataSource()
+    {
+        $getDataSource = self::getMethod('getDataSource');
+        $action = new \Phruts\Action();
+        $mockActionKernel = new MockActionKernel();
+        $action->setActionKernel($mockActionKernel);
+        $datasource = $getDataSource->invokeArgs($action, array(new \Symfony\Component\HttpFoundation\Request(), 'key'));
+        $this->assertNotEmpty($datasource);
+    }
+
+
+    protected static function getMethod($name)
+    {
+        $class = new ReflectionClass('\Phruts\Action');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
 }
 
 
 class MockActionKernel extends \Phruts\Action\ActionKernel
+{
+
+    function __construct()
+    {
+    }
+
+    public function getDataSource(\Symfony\Component\HttpFoundation\Request $request, $key)
+    {
+        return new MockPDO();
+    }
+
+
+}
+
+class MockPDO extends \PDO
 {
 
     function __construct()
